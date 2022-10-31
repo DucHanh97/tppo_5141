4ef5687892268c6fe4d5c169ea99a3dfa4aa4a21
Đức Hạnh Nguyễn <nguyenhanhthucquy@gmail.com>
2022-10-31 22:18:11 +0300

    Add foder with file for lab 2 tppo

---
+++ lab2_tppo_5141/Device.py
@@ -0,0 +1,37 @@
+from tkinter import Canvas
+
+def getCanvas():
+    device = open('Device.txt', 'r')
+    CanLine = device.readline()
+    device.close()
+    for i in range(len(CanLine)):
+        if CanLine[i] == '=':
+            return int(CanLine[i+1:])
+
+def getLightFlow():
+    device = open('Device.txt', 'r')
+    device.readline()
+    LightFlow = device.readline()
+    device.close()
+    for i in range(len(LightFlow)):
+        if LightFlow[i] == '=':
+            return int(LightFlow[i+1:])
+
+def getIllumination():
+    device = open('Device.txt', 'r')
+    device.readline()
+    device.readline()
+    Illumination = device.readline()
+    device.close()
+    for i in range(len(Illumination)):
+        if Illumination[i] == '=':
+            return int(Illumination[i+1:])
+
+def setParams(canvas, lightflow):
+    illumin = str(setIllumination(int(canvas), int(lightflow)))
+    device = open('Device.txt','w')
+    device.writelines("canvas="+canvas+"\nlightflow="+lightflow+"\nillumination="+illumin)
+    device.close()
+
+def setIllumination(canvas, light):
+    return int((canvas * light) / 10000 * 50000)
