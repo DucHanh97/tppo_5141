b90c130e360d55f7f9b0911a0f396b3ce3e9340b
Đức Hạnh Nguyễn <nguyenhanhthucquy@gmail.com>
2022-11-06 22:07:18 +0300

    Clients receive from server, threading sends device changes to clients

---
+++ lab2_tppo_5141/tppo_client_5141.py
@@ -0,0 +1,89 @@
+import socketio
+import requests
+
+commands = ("set", "setcanvas", "setlight", "get")
+
+client_io = socketio.Client()
+
+def get_parameter():
+    global host
+    global port
+    api_url = f"http://{host}:{port}/blinds/infor"
+    return requests.get(api_url)
+
+def set_parameter(parameter, value):
+    global host
+    global port
+    api_url = f"http://{host}:{port}/blinds?{parameter}={value}"
+    return requests.get(api_url)
+
+def set_both_parameters(param_1, value_1, param_2, value_2):
+    global host
+    global port
+    api_url = f"http://{host}:{port}/blinds?{param_1}={value_1}&{param_2}={value_2}"
+    return requests.get(api_url)
+
+def strtoint(str):
+    try:
+        return(int(str))
+    except:
+        return -1
+
+@client_io.event
+def connect():
+    global host
+    global port
+    print(">>>>>Successful connection to the TCP_Server<<<<<")
+    print("**[List of supported commands]**")
+    print("> set <value> <value> - Set shift canvan and light flow")
+    print("> setcanvas <value> - Set shift canvas")
+    print("> setlight <value> - Set light flow")
+    print("> 0 <= value <= 100")
+    print("> get - Read the values of all parameters of the blind")
+    print("> !DISCONNECT - disconnect to the TCP_Server")
+    print("------------------------------------------------------\n")
+    print("Enter a command\n")
+    while True:
+        cmd = input()
+        buff_cmd = cmd.split()
+        if buff_cmd[0] not in commands:
+            print("Command is not supported")
+        elif buff_cmd[0] == commands[0]:
+            if len(buff_cmd) < 3 or len(buff_cmd) > 3:
+                print("Enter command according to the form: set <value> <value>")
+            elif strtoint(buff_cmd[1]) < 0 or strtoint(buff_cmd[1]) > 100 or strtoint(buff_cmd[2]) < 0 or strtoint(buff_cmd[2]) > 100:
+                print("Enter values between 0 and 100")
+            else:
+                response = set_both_parameters("canvas",buff_cmd[1],"light", buff_cmd[2])
+                print(f"[Response from server]: ", response.json())
+        elif buff_cmd[0] == commands[1]:
+            if len(buff_cmd) < 2 or len(buff_cmd) > 2:
+                print("Enter command according to the form: setcanvas <value>")
+            elif strtoint(buff_cmd[1]) < 0 or strtoint(buff_cmd[1]) > 100:
+                print("Enter a value between 0 and 100")
+            else:
+                response = set_parameter("canvas", buff_cmd[1])
+                print(f"[Response from server]: ", response.json())
+        elif buff_cmd[0] == commands[2]:
+            if len(buff_cmd) < 2 or len(buff_cmd) > 2:
+                print("Enter command according to the form: setlight <value>")
+            elif strtoint(buff_cmd[1]) < 0 or strtoint(buff_cmd[1]) > 100:
+                print("Enter a value between 0 and 100")
+            else:
+                response = set_parameter("light", buff_cmd[1])
+                print(f"[Response from server]: ", response.json())
+        else:
+            response = get_parameter()
+            print(f"[Response from server]: ", response.json())
+
+@client_io.on("DeviceChanged")
+def DeviceChanged_Handler(Notice):
+    print(f"[NOTIFICATION FROM THE SERVER]: {Notice}")
+
+@client_io.event
+def disconnect():
+    print("Disconnected to the Server")
+
+host = input("Enter Server IP_Address: ")
+port = input("Enter Server PORT: ")
+client_io.connect(f"http://{host}:{port}")
--- lab2_tppo_5141/tppo_server_5141.py
+++ lab2_tppo_5141/tppo_server_5141.py
@@ -1,7 +1,6 @@
 import Device as device
-import socket
+from socket import SocketIO
 import threading
-import requests
 from flask_socketio import SocketIO
 from flask import Flask, request, abort
 import os
@@ -20,19 +19,9 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allower_origins="*", logger=True, engineio_logger=True, async_handlers=True)
 
 #----------- Set route for REST APIs ------------
-@app.route('/blinds/<string:parameter>')
-def main(parameter):
-    print(parameter)
-    if(parameter == "canvas"):
-        value = device.getCanvas()
-    elif(parameter == "light"):
-        value = device.getLightFlow()
-    elif(parameter == "illumin"):
-        value = device.getIllumination()
-    else:
-        abort(404)
-        return "Parameter is not support"
-    return  {parameter: value}
+@app.route('/blinds/infor')
+def main():
+    return Dv_Params
 
 @app.route('/blinds')       #Query String, http://ip_host/blinds?<parameter>=<value>
 def get_query_string():
@@ -42,23 +31,22 @@ def get_query_string():
     global Dv_Params
     canvas = request.args.get('canvas')
     light = request.args.get('light')
-    print("before if: canvas= ",canvas, "light= ", light, type(canvas), type(light))
+    
     if((canvas != None) & (light == None)):
-        print(canvas, type(canvas))
         device.setParams(canvas, cur_Light)
         cur_Canvas = str(device.getCanvas())
+        cur_Light = str(device.getLightFlow())
         cur_Illumin = str(device.getIllumination())
         Dv_Params = [{"canvas":cur_Canvas}, {"lightflow":cur_Light}, {"illumination":cur_Illumin}]
         return Dv_Params
     elif((light != None) & (canvas == None)):
-        print(light, type(light))
         device.setParams(cur_Canvas, light)
+        cur_Canvas = str(device.getCanvas())
         cur_Light = str(device.getLightFlow())
         cur_Illumin = str(device.getIllumination())
         Dv_Params = [{"canvas":cur_Canvas}, {"lightflow":cur_Light}, {"illumination":cur_Illumin}]
         return Dv_Params
     elif((canvas != None) & (light != None)):
-        print("canvas = ",canvas, "light = ", light)
         device.setParams(canvas, light)
         cur_Canvas = str(device.getCanvas())
         cur_Light = str(device.getLightFlow())
@@ -81,6 +69,7 @@ def client_disconnect():
 
 #----------- Notify device changes to clients ----------
 def notifDeviceChange_handle():
+    global socketio
     global cur_Canvas
     global cur_Light
     global cur_Illumin
@@ -91,18 +80,21 @@ def notifDeviceChange_handle():
         if time_change != time_cache:
             time_cache = time_change
             
-            cur_Canvas  = device.getCanvas()
-            cur_Light   = device.getLightFlow()
+            cur_Canvas  = str(device.getCanvas())
+            cur_Light   = str(device.getLightFlow())
 
-            if cur_Canvas != Dv_Params["canvas"] or cur_Light != Dv_Params["lightflow"]:
-                device.setParams(str(cur_Canvas), str(cur_Light))
+            if (cur_Canvas != Dv_Params[0]["canvas"]) or (cur_Light != Dv_Params[1]["lightflow"]):
+                print("Device had changed")
+                device.setParams(cur_Canvas, cur_Light)
                 Dv_Params[0]["canvas"] = str(device.getCanvas())
                 Dv_Params[1]["lightflow"] = str(device.getLightFlow())
                 Dv_Params[2]["illumination"] = str(device.getIllumination())
-                socketio.emit("Notification: Device parameters have changed", Dv_Params, broadcast=True)
+                print(Dv_Params)
+                socketio.emit("Notification: Device parameters have changed: {Dv_Params}", broadcast=True)
                 print("Sent data to clients")
 
-notifDeviceChange = threading.Thread(target=notifDeviceChange_handle)
+notifDeviceChange = threading.Thread(target=notifDeviceChange_handle, args=())
+notifDeviceChange.start()
 
 if(__name__ == "__main__"):
     socketio.run(app, host=argv[1], port=int(argv[2]), debug=False)
